5f99f377e73a4ec69f04ff5f2878a6fe39177f5d
Deepak Sharma <dpkshrma01@gmail.com>
2018-02-12 19:29:53 +0530

    Add callback to the setState

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const promisifySetState = (WrappedComponent) => {
-  WrappedComponent.prototype.originalSetState = WrappedComponent.prototype.setState;
-  WrappedComponent.prototype.setState = function(partialState) {
+  WrappedComponent.prototype.originalSetState = WrappedComponent.prototype.setState; // eslint-disable-line
+  WrappedComponent.prototype.setState = function (partialState, callback) { // eslint-disable-line
     return new Promise((resolve) => {
       this.originalSetState(partialState, (...args) => {
+        if (callback) callback(args);
         resolve(args);
       });
     });
